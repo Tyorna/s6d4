373212ae8ac998a92b44fdcc632cfc963ec25f41
Francesca Arata <francescaarata@live.it>
2023-08-24 16:46:33 +0200

    inizio base

---
+++ src/main/java/com/example/demo/UserData.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class UserData {
+	 private String nomeCompleto; 
+	 private int eta; 
+	 public void getData(DataSource ds) { 
+	 nomeCompleto = ds. getNomeCompleto (); 
+	 eta = ds.getEta(); 
+}
+}
